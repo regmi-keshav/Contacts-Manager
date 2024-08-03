617794781eec7fce55e3d18b54e9002984e4fcd6
keshav_regmi <keshavregmi70@gmail.com>
2024-08-03 19:17:57 +0530

    Added functionality for each API endpoints

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -5,14 +5,19 @@ const Contact = require("../models/contactModel");
 //@access public
 
 const getContacts = asyncHandler(async (req, res) => {
-  console.log("hello");
-  res.status(500).json({ message: "Get Contacts" });
+  const contact = await Contact.find({});
+  res.status(200).json(contact);
 });
 //@desc Get Contact
 //@route GET /api/contacts/:id
 //@access public
 const getContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get Contacts ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact Not Found !");
+  }
+  res.status(200).json(contact);
 });
 
 //@desc Create Contacts
@@ -25,20 +30,32 @@ const createContacts = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are mandatory !");
   }
-  res.status(201).json({ message: "Create Contacts" });
+  res.status(201).json(contact);
 });
 
 //@desc Update Contacts
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update Contacts ${req.params.id}` });
+  const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact Not Found !");
+  }
+  res.status(200).json(contact);
 });
 
 //@desc Delete Contacts
 //@route DELETE /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findByIdAndDelete(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact Not Found !");
+  }
   res.status(200).json({ message: `Delete Contacts ${req.params.id}` });
 });
 
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const app = express();
 
 const contactRouters = require("./routes/contactRouters");
+const userRouters = require("./routes/userRouters");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 
 connectDb();
 app.use(express.json());
 app.use("/api/contacts/", contactRouters);
+app.use("/api/users/", userRouters);
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
